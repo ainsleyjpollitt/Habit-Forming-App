a6f536a88d5646ccf976cc9bdc0ec08261ff4202
Ainsley Pollitt <ainsleyjpollitt@gmail.com>
2022-11-17 14:19:17 -0800

    Allowed for input of task name on Task Creation activity, sends input to Tasks Activity but needs to have a place to store it

--- app/src/main/java/com/example/habitformingapp/TaskCreationActivity.java
+++ app/src/main/java/com/example/habitformingapp/TaskCreationActivity.java
@@ -2,7 +2,16 @@ package com.example.habitformingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnKeyListener;
+import android.view.View;
+import android.view.KeyEvent;
 
 public class TaskCreationActivity extends AppCompatActivity {
 
@@ -10,5 +19,21 @@ public class TaskCreationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_creation);
+        findViewById(R.id.editTaskName).setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent keyevent) {
+                if ((keyevent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    enterNewTaskName(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void enterNewTaskName(View v) {
+        Intent i = new Intent(this, TasksActivity.class);
+        String taskName = ((EditText)v).getText().toString();
+        i.putExtra("TASK_NAME", taskName);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/habitformingapp/TasksActivity.java
+++ app/src/main/java/com/example/habitformingapp/TasksActivity.java
@@ -11,4 +11,6 @@ public class TasksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
     }
+
+
 }
