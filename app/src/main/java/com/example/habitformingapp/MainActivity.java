8d72d07a8708401ccbaef4efbea6ecffd1021f0d
Ainsley Pollitt <ainsleyjpollitt@gmail.com>
2022-11-17 12:00:23 -0800

    Set up the Home page for nav to other pages, can click to pages but they need renames/go back feature

--- app/src/main/java/com/example/habitformingapp/MainActivity.java
+++ app/src/main/java/com/example/habitformingapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.habitformingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Home");
+    }
+
+    public void launchTasks(View v) {
+        Intent i = new Intent(this, TasksActivity.class);
+        startActivity(i);
+    }
+
+    public void launchCalendar(View v) {
+        Intent i = new Intent(this, CalendarActivity.class);
+        startActivity(i);
+    }
+
+    public void launchTaskCreation(View v) {
+        Intent i = new Intent(this, TaskCreationActivity.class);
+        startActivity(i);
+    }
+
+    public void launchStats(View v) {
+        Intent i = new Intent(this, StatisticsActivity.class);
+        startActivity(i);
     }
 }
